ba4fdc3d2f970b2826bc4154225d91e2fb20ea59
AgurSan <clement.sananikone@gmail.com>
2024-05-10 11:41:16 +0200

    modified:   src/ressources/metrics/services/index.ts new file:   src/ressources/metrics/services/total-revenue.service.ts
    Création du service TotalRevenueService :
    
    - Import de DataService depuis './data.service' et de CsvParser depuis '~/utils'.
    - Définition de la classe TotalRevenueService avec un constructeur prenant un paramètre DataService.
    - Méthode getTotalRevenue : Calcule le revenu total en multipliant les ventes et la quantité de chaque article dans les données.

--- src/ressources/metrics/services/index.ts
+++ src/ressources/metrics/services/index.ts
@@ -1,1 +1,1 @@
-export * from './csv.parser';
+export * from './total-revenue.service';
---
+++ src/ressources/metrics/services/total-revenue.service.ts
@@ -0,0 +1,17 @@
+import { DataService } from './data.service';
+import { CsvParser } from '~/utils'; 
+
+export class TotalRevenueService {
+    constructor(private dataService: DataService) {}
+
+    /**
+     * Calculates the total revenue by multiplying the sales and quantity of each item in the data.
+     *
+     * @return {Promise<number>} The total revenue.
+     */
+    async getTotalRevenue(): Promise<number> {
+        const data = await this.dataService.getData();
+        const totalRevenue = data.reduce((acc, item) => acc + item.Sales * item.Quantity, 0);
+        return totalRevenue;
+    }
+}
