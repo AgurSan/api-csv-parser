144b3b8fc53417c97dcdde3303eca0e76069ed59
AgurSan <clement.sananikone@gmail.com>
2024-05-10 11:02:12 +0200

    modified:   src/ressources/metrics/services/csv.parser.ts
    filePath en param pour rendre plus flexible le parsing de csv

--- src/ressources/metrics/services/csv.parser.ts
+++ src/ressources/metrics/services/csv.parser.ts
@@ -3,37 +3,38 @@ import fs from 'fs';
 import csv from 'csv-parser';
 
 class CsvParser {
-    /**
-     * Parses the data from a CSV file and returns an array of Product objects.
-     *
-     * @return {Promise<Product[]>} A promise that resolves to an array of Product objects.
-     */
-    async parseData(): Promise<Product[]> {
+/**
+ * Parses the data from a CSV file and returns an array of Product objects.
+ *
+ * @param filePath The path to the CSV file.
+ * @returns A promise that resolves to an array of Product objects.
+ */
+async parseData(filePath: string): Promise<Product[]> {
     return new Promise<Product[]>((resolve, reject) => {
         const datas: Product[] = [];
-        const stream = fs.createReadStream('public/dataset.csv');
+        const stream = fs.createReadStream(filePath);
 
         stream.pipe(csv({ mapHeaders: ({ header }) => header.trim() }))
-        .on('data', (data) => {
-            const rowData: Product = {
-            'Row ID': parseInt(data['Row ID']),
-            'Order ID': data['Order ID'],
-            'Order Date': new Date(Date.parse(data['Order Date'])),
-            'Customer ID': data['Customer ID'],
-            'State': data['State'],
-            'Region': data['Region'],
-            'Product ID': data['Product ID'],
-            'Sales': parseFloat(data['Sales']),
-            'Quantity': parseInt(data['Quantity'])
-            };
-            datas.push(rowData);
-        })
-        .on('end', () => {
-            resolve(datas);
-        })
-        .on('error', (error) => {
-            reject(error);
-        });
+            .on('data', (data: any) => {
+                const rowData: Product = {
+                    'Row ID': parseInt(data['Row ID']),
+                    'Order ID': data['Order ID'],
+                    'Order Date': new Date(Date.parse(data['Order Date'])),
+                    'Customer ID': data['Customer ID'],
+                    'State': data['State'],
+                    'Region': data['Region'],
+                    'Product ID': data['Product ID'],
+                    'Sales': parseFloat(data['Sales']),
+                    'Quantity': parseInt(data['Quantity'])
+                };
+                datas.push(rowData);
+            })
+            .on('end', () => {
+                resolve(datas);
+            })
+            .on('error', (error: Error) => {
+                reject(error);
+            });
     });
-    }
+}
 }
