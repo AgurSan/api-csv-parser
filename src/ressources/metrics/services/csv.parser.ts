03206859bb873d121e212880d30d554f2b5aff2d
AgurSan <clement.sananikone@gmail.com>
2024-05-10 10:51:55 +0200

    new file:   src/ressources/metrics/services/csv.parser.ts new file:   src/ressources/metrics/services/index.ts
    class CsvParser done
    export par services/index.ts
    JSDoc

---
+++ src/ressources/metrics/services/csv.parser.ts
@@ -0,0 +1,39 @@
+import type { Product } from "~~/types/Product";
+import fs from 'fs';
+import csv from 'csv-parser';
+
+class CsvParser {
+    /**
+     * Parses the data from a CSV file and returns an array of Product objects.
+     *
+     * @return {Promise<Product[]>} A promise that resolves to an array of Product objects.
+     */
+    async parseData(): Promise<Product[]> {
+    return new Promise<Product[]>((resolve, reject) => {
+        const datas: Product[] = [];
+        const stream = fs.createReadStream('public/dataset.csv');
+
+        stream.pipe(csv({ mapHeaders: ({ header }) => header.trim() }))
+        .on('data', (data) => {
+            const rowData: Product = {
+            'Row ID': parseInt(data['Row ID']),
+            'Order ID': data['Order ID'],
+            'Order Date': new Date(Date.parse(data['Order Date'])),
+            'Customer ID': data['Customer ID'],
+            'State': data['State'],
+            'Region': data['Region'],
+            'Product ID': data['Product ID'],
+            'Sales': parseFloat(data['Sales']),
+            'Quantity': parseInt(data['Quantity'])
+            };
+            datas.push(rowData);
+        })
+        .on('end', () => {
+            resolve(datas);
+        })
+        .on('error', (error) => {
+            reject(error);
+        });
+    });
+    }
+}
