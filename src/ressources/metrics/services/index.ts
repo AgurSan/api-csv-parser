671e4f868687da77018c6288937ce903e9dc12dd
AgurSan <clement.sananikone@gmail.com>
2024-05-11 08:36:27 +0200

    modified:   src/ressources/metrics/services/index.ts
    Mise à jour des exports de services.

--- src/ressources/metrics/services/index.ts
+++ src/ressources/metrics/services/index.ts
@@ -1,1 +1,6 @@
+export * from './avg-revenue-per-order.service';
+export * from './data-by-date.service';
+export * from './data-by-state.service';
+export * from './num-orders.service';
 export * from './total-revenue.service';
+export * from './unique-customers.service';
