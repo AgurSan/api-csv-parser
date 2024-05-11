8afcd6b42ad5b033814c56003b57800607b1af3e
AgurSan <clement.sananikone@gmail.com>
2024-05-11 11:54:50 +0200

    build: handler + routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import { config } from "~/config";
 import {
     AvgRevenuePerOrderController,
@@ -13,8 +13,41 @@ import {
 const app: Express = express();
 app.use(cors());
 
+//handler
+export async function handleRequest(
+    request: Request,
+    response: Response,
+    action: (request: Request) => Promise<any>
+): Promise<void> {
+    try {
+        const result = await action(request);
+        response.status(200).json(result);
+    } catch (error: any) {
+        response.status(500).json({ error: error.message });
+    }
+}
 
-//TODO: routes
+//routes
+type ControllerMethod = (req: Request, res: Response) => Promise<void>;
+
+const endpointMap: Record<string, ControllerMethod> = {
+    "avg-revenue-per-order": AvgRevenuePerOrderController.getAvgRevenuePerOrder,
+    "data-by-date": DataByDateController.getDataByDate,
+    "data-by-state": DataByStateController.getDataByState,
+    "num-orders": NumOrdersController.getNumOrders,
+    "total-revenue": TotalRevenueController.getTotalRevenue,
+    "unique-customers": UniqueCustomersController.getUniqueCustomers
+};
+
+app.get("/:endpoint", async (req: Request, res: Response) => {
+    const controllerMethod = endpointMap[req.params.endpoint];
+    if (controllerMethod) {
+        return controllerMethod(req, res);
+    } else {
+        res.status(404).json({ error: "Not found" });
+    }
+});
+
 
 //TODO: Gestion des erreurs
 
