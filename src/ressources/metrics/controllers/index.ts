f69400de18ea990222e5e5d1e8c75dd2023e986b
AgurSan <clement.sananikone@gmail.com>
2024-05-11 10:45:58 +0200

    fix d'un oublie d'export

--- src/ressources/metrics/controllers/index.ts
+++ src/ressources/metrics/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './data-by-date.controller';
 export * from './data-by-state.controller';
 export * from './num-orders.controller';
 export * from './total-revenue.controller';
+export * from './unique-customers.controller';
