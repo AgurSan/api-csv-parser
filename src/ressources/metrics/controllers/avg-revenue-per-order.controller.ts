be8293db34ff15f6a058ea4c90a813f8e32b3685
AgurSan <clement.sananikone@gmail.com>
2024-05-11 11:25:09 +0200

    feat: Convert controller methods to static

--- src/ressources/metrics/controllers/avg-revenue-per-order.controller.ts
+++ src/ressources/metrics/controllers/avg-revenue-per-order.controller.ts
@@ -3,16 +3,18 @@ import { AvgRevenuePerOrderService } from '../services/avg-revenue-per-order.ser
 import { DataService } from '~/utils';
 
 export class AvgRevenuePerOrderController {
-    private avgRevenuePerOrderService: AvgRevenuePerOrderService;
+    private static avgRevenuePerOrderService: AvgRevenuePerOrderService;
 
     /**
      * Constructs a new instance of the AvgRevenuePerOrderController class.
      *
      * @param {DataService} dataService - The data service used to retrieve data.
+     * @return {void}
      */
     constructor(private dataService: DataService) {
-        this.avgRevenuePerOrderService = new AvgRevenuePerOrderService(dataService);
+        AvgRevenuePerOrderController.avgRevenuePerOrderService = new AvgRevenuePerOrderService(dataService);
     }
+
 
     /**
      * Retrieves the average revenue per order.
@@ -21,12 +23,13 @@ export class AvgRevenuePerOrderController {
      * @param {Response} res - The response object.
      * @return {Promise<void>} - A promise that resolves when the average revenue per order is retrieved and the response is sent.
      */
-    async getAvgRevenuePerOrder(req: Request, res: Response) {
+    static async getAvgRevenuePerOrder(req: Request, res: Response) {
         try {
-            const avgRevenuePerOrder = await this.avgRevenuePerOrderService.getAvgRevenuePerOrder();
+            const avgRevenuePerOrder = await AvgRevenuePerOrderController.avgRevenuePerOrderService.getAvgRevenuePerOrder();
             res.status(200).json({ avgRevenuePerOrder });
         } catch (error: any) {
             res.status(500).json({ error: error.message });
         }
     }
 }
+
