518fa5f2114396537af837554282507c8f23f1ee
AgurSan <clement.sananikone@gmail.com>
2024-05-11 10:36:25 +0200

    build: controllers/total-revenue.controller.ts

---
+++ src/ressources/metrics/controllers/total-revenue.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { TotalRevenueService } from '../services/total-revenue.service';
+import { DataService } from '~/utils';
+
+export class TotalRevenueController {
+    private totalRevenueService: TotalRevenueService;
+
+    /**
+     * Constructs a new instance of the TotalRevenueController class.
+     *
+     * @param {DataService} dataService - The data service used to retrieve data.
+     */
+    constructor(private dataService: DataService) {
+        this.totalRevenueService = new TotalRevenueService(dataService);
+    }
+
+    /**
+     * Retrieves the total revenue by multiplying the sales and quantity of each item in the data.
+     *
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     * @return {Promise<void>} - A promise that resolves when the total revenue is retrieved and the response is sent.
+     */
+    async getTotalRevenue(req: Request, res: Response): Promise<void> {
+        try {
+            const totalRevenue = await this.totalRevenueService.getTotalRevenue();
+            res.status(200).json({ totalRevenue });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+}
