30b3cf3f54a0c4a99908f21bb6a7716c901b2418
AgurSan <clement.sananikone@gmail.com>
2024-05-13 14:46:51 +0200

    JSDoc

--- src/ressources/metrics/services/data-by-state.service.ts
+++ src/ressources/metrics/services/data-by-state.service.ts
@@ -3,6 +3,12 @@ import { Product } from '~~/types/Product';
 
 export class DataByStateService {
     constructor(private dataService: DataService) {}
+    /**
+     * Retrieves data filtered by the specified state.
+     *
+     * @param {string} state - The state to filter the data by.
+     * @return {Promise<Product[]>} The data filtered by the specified state.
+     */
     async getDataByState(state: string): Promise<Product[]> {
         const data = await this.dataService.getData();
         const filteredData = data.filter(item => item['State'] === state);
