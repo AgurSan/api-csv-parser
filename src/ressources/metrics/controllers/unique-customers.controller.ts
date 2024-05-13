a8ff807c8dda6edca69fcaf0423826b816148dff
AgurSan <clement.sananikone@gmail.com>
2024-05-13 10:48:11 +0200

    correction du controller unique-customers.controller.ts

--- src/ressources/metrics/controllers/unique-customers.controller.ts
+++ src/ressources/metrics/controllers/unique-customers.controller.ts
@@ -1,32 +1,16 @@
-import { Request, Response } from 'express';
 import { UniqueCustomersService } from '../services/unique-customers.service';
-import { DataService } from '~/utils';
+import { CsvParser, DataService } from '~/utils';
+import { config } from "~/config";
 
-export class UniqueCustomersController {
-    private static uniqueCustomersService: UniqueCustomersService;
+const csvParser = new CsvParser();
+const dataService = new DataService(csvParser, config.CSV_FILE_PATH);
+const uniqueCustomersService = new UniqueCustomersService(dataService);
 
-    /**
-     * Constructs a new instance of the UniqueCustomersController class.
-     *
-     * @param {DataService} dataService - The data service used to retrieve data.
-     */
-    constructor(private dataService: DataService) {
-        UniqueCustomersController.uniqueCustomersService = new UniqueCustomersService(dataService);
-    }
-
-    /**
-     * Retrieves the number of unique customers asynchronously.
-     *
-     * @param {Request} req - The request object.
-     * @param {Response} res - The response object.
-     * @return {Promise<void>} A promise that resolves when the number of unique customers is retrieved and the response is sent.
-     */
-    static async getUniqueCustomers(req: Request, res: Response): Promise<void> {
-        try {
-            const uniqueCustomers = await UniqueCustomersController.uniqueCustomersService.getUniqueCustomers();
-            res.status(200).json({ uniqueCustomers });
-        } catch (error: any) {
-            res.status(500).json({ error: error.message });
-        }
+export const UniqueCustomersController = {
+    
+    getUniqueCustomers: async () => {
+        const uniqueCustomers = await uniqueCustomersService.getUniqueCustomers();
+        return uniqueCustomers;
     }
 }
+
