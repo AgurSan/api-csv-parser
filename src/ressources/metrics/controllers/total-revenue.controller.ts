48e94f783cbd11e14d6f05aa68beaca0a6dcac61
AgurSan <clement.sananikone@gmail.com>
2024-05-13 10:42:24 +0200

    feat: JSDoc

--- src/ressources/metrics/controllers/total-revenue.controller.ts
+++ src/ressources/metrics/controllers/total-revenue.controller.ts
@@ -8,6 +8,11 @@ const dataService = new DataService(csvParser, config.CSV_FILE_PATH);
 const totalRevenueService = new TotalRevenueService(dataService);
 
 export const TotalRevenueController = {
+    /**
+     * Retrieves the total revenue by calling the calculateTotalRevenue method of the totalRevenueService.
+     *
+     * @return {Promise<number>} The total revenue calculated by the calculateTotalRevenue method.
+     */
     getTotalRevenue: async () => {
         const totalRevenue = await totalRevenueService.calculateTotalRevenue();
         return totalRevenue;    
